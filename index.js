d00f34e2499ff411ce9525925353201a9eab9fda
aryan <aryansadeghyan54@gmail.com>
2024-06-12 16:23:30 +0330

    adding cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 const prisma = new PrismaClient();
 app.use(express.json());
